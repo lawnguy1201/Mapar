7b97457c599747d524c9b6b1fd35b6429c6155bd
Lawnguy <lawnguygithub@gmail.com>
2026-01-10 12:20:39 -0500

    init commit

---
+++ src/main/java/com/example/addon/modules/QuickEmpty.java
@@ -0,0 +1,5 @@
+package com.example.addon.modules;
+
+//Empty out our entire inv add a setting to not do hotbar
+public class QuickEmpty {
+}
---
+++ src/main/java/com/example/addon/modules/SmartVerifier.java
@@ -0,0 +1,5 @@
+package com.example.addon.modules;
+// hook up with litematica to verify the schematic, put up beacon beam where they are missing carpets/blocks
+// need to add setting to not to scaffolding blocks for staircasing since the main use is staircase maps
+public class SmartVerifier {
+}
